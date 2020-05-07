86a3666b8e1877770f5cfa3f1e4c224aac99959a
Luis Correal Viveros <49031153+Golai@users.noreply.github.com>
2020-05-06 23:23:04 -0500

    Update ILogin.java

--- src/interfaces/ILogin.java
+++ src/interfaces/ILogin.java
@@ -1,9 +1,10 @@
 package interfaces;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 
-public interface ILogin {
+public interface ILogin extends Remote{
 	boolean registrarUsu(int cedula, String nombre, String password, String email, int celular) throws RemoteException;
 	boolean login(int cedula, String password)  throws RemoteException;
 
