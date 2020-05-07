5f2d779d52592f427fd6b880f6ff4f85558ee969
Luis Correal Viveros <49031153+Golai@users.noreply.github.com>
2020-05-06 23:23:40 -0500

    Update IVisualizarR.java

--- src/interfaces/IVisualizarR.java
+++ src/interfaces/IVisualizarR.java
@@ -1,9 +1,10 @@
 package interfaces;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 import persistence.VisualizarR;;
 
-public interface IVisualizarR {
+public interface IVisualizarR extends Remote {
 	VisualizarR listarRanking() throws RemoteException;
 }
